b748409eadde8617a44e26f83f5ef138a865630f
JerinXplore <jerinjerin101325@gmail.com>
2025-11-09 02:28:56 +0600

    setup TypeScript directly with Node 22.11.0

---
+++ dist/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "Next Level Web Development";
+console.log(course); //22.11.0 version thke typescript javascript er moto korei run kore..
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+const course = "Next Level Web Development";
+
+console.log(course);//22.11.0 version thke typescript javascript er moto korei run kore..
